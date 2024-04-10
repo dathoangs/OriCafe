4c798e262595a9c937db10187083ddd7d22cf1e2
Dat Hoang <q.dathoangs@gmail.com>
2024-04-10 23:25:18 +0700

    Cập nhật 10-04

---
+++ Models/OrderingItem.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace OriCafe.Models
+{
+    public class OrderingItem{
+        public int IdSanPham { get; set; }
+        public string TenSanPham { get; set; }
+        public string LoaiSanPham { get; set; }
+        public float GiaBan { get; set; }
+
+        public int SoLuong { get; set; }
+
+        public OrderingItem(int IdSanPham, string TenSanPham, string LoaiSanPham, float GiaBan, int SoLuong){
+            this.IdSanPham = IdSanPham;
+            this.TenSanPham = TenSanPham;
+            this.LoaiSanPham = LoaiSanPham;
+            this.GiaBan = GiaBan;
+            this.SoLuong = SoLuong;
+        }
+    }
+}
