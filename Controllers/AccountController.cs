8335ddda83a0c4192502613c9df1e34e0c01f204
Dat Hoang <q.dathoangs@gmail.com>
2024-04-17 08:07:45 +0700

    Cập nhật 17-4

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,62 @@
+﻿using OriCafe.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AnonymousForum.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> _logger;
+        public OriCafeContext db;
+
+        public AccountController (ILogger<AccountController> logger)
+        {
+            _logger = logger;
+            db = new OriCafeContext();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }        
+
+        // GET: Accounts
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string username, string password)
+        {
+            var userData = db.TaiKhoans;
+            bool isValidUser = false;
+
+            foreach (TaiKhoan taiKhoan in userData){
+                if(username == taiKhoan.TenTaiKhoan && password == taiKhoan.MatKhau){
+                    isValidUser = true;
+                    ViewBag.taiKhoan = taiKhoan;
+                }     
+            }
+
+            if (isValidUser)
+            {
+                HttpContext.Session.SetString("IsAuthenticated", "true");
+                HttpContext.Session.SetString("Username", username);
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.ErrorMessage = "Invalid username or password.";
+            return RedirectToAction("Index", "Account");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Logout()
+        {
+            // HttpContext.Session.Remove("IsAuthenticated");
+            // ViewBag.taiKhoan = null;
+            return RedirectToAction("Index", "Account");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(5);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -20,13 +27,14 @@ app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
+app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Account}/{action=Index}/{id?}");
 
-app.UseEndpoints(endpoints => {
     
-});
+
+
 
 app.Run();
